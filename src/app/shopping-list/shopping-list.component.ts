6482f8b58a883593c229e053241eb9d9c5f251d4
Santeri Rusila <santtu_1993@hotmail.com>
2019-08-30 17:20:42 +0300

    Forms

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css'],
+})
+export class ShoppingListComponent implements OnInit, OnDestroy {
+  ingredients: Ingredient[];
+  private subscription: Subscription;
+  private startedEditingSubscription: Subscription;
+  private editItemIndex: number;
+
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  ngOnInit() {
+    this.ingredients = this.shoppingListService.getIngredients();
+    this.subscription = this.shoppingListService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+        this.editItemIndex = null;
+      }
+    );
+    this.startedEditingSubscription = this.shoppingListService.startedEditing.subscribe(
+      (index: number) => {
+        console.log(index);
+        this.editItemIndex = index;
+      }
+    );
+  }
+
+  onEditItem(index: number) {
+    this.shoppingListService.startedEditing.next(index);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}
